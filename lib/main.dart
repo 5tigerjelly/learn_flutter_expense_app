918181d37cd01717a4b4d8bcf754474c99d80a21
Chris Oh <chris@5tigerjelly.com>
2023-02-24 17:00:31 -0500

    init

---
+++ lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import './transaction.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: 't1', title: 'New Shoe', amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: 't2',
+        title: 'Weekly Grocierys',
+        amount: 16.53,
+        date: DateTime.now())
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flutter App'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            width: double.infinity,
+            child: const Card(
+              color: Colors.blue,
+              elevation: 5,
+              child: Text('CHART!'),
+            ),
+          ),
+          Column(
+              children: transactions.map((e) {
+            return Card(
+              child: Row(
+                children: <Widget>[
+                  Container(child: Text(e.amount.toString())),
+                  Column(
+                    children: <Widget>[Text(e.title), Text(e.date.toString())],
+                  )
+                ],
+              ),
+            );
+          }).toList())
+        ],
+      ),
+    );
+  }
+}
